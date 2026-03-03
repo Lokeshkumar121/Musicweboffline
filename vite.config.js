f56662b9b3023b538eddd49b0745b90fe4a93392
Lokesh Kumar <lokeshk3875@gmail.com>
2026-03-03 15:34:36 +0530

    Fixed large audio caching

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["favicon.svg", "robots.txt"],
+      includeAssets: ["app-icon.png"],
       manifest: {
         name: "My Music Player",
         short_name: "MusicApp",
@@ -18,20 +18,47 @@ export default defineConfig({
         start_url: "/",
         icons: [
           {
-            src: "/images/img1.jpg",
+            src: "/app-icon.png",
             sizes: "192x192",
             type: "image/png"
           },
           {
-            src: "/images/img1.jpg",
+            src: "/app-icon.png",
             sizes: "512x512",
             type: "image/png"
           }
         ]
       },
       workbox: {
-        globPatterns: ["**/*.{js,css,html,mp3,jpg,png}"]
+  globPatterns: ["**/*.{js,css,html,jpg,png,svg}"],
+
+  maximumFileSizeToCacheInBytes: 20 * 1024 * 1024, // 20 MB limit
+
+  runtimeCaching: [
+    {
+      urlPattern: ({ request }) =>
+        request.destination === "audio",
+      handler: "CacheFirst",
+      options: {
+        cacheName: "audio-cache",
+        expiration: {
+          maxEntries: 50
+        }
       }
+    },
+    {
+      urlPattern: ({ request }) =>
+        request.destination === "image",
+      handler: "CacheFirst",
+      options: {
+        cacheName: "image-cache",
+        expiration: {
+          maxEntries: 50
+        }
+      }
+    }
+  ]
+}
     })
   ]
 });
